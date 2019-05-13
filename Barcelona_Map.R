7cd599a9f57d127db25b929410116fa37561014c
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-13 23:17:14 +0200

    Add files via upload

---
+++ Barcelona_Map.R
@@ -0,0 +1,30 @@
+
+
+# load necessary packages
+library( geojsonio )
+
+# transfrom .json file into a spatial polygons data frame
+states <- 
+  geojson_read( 
+    x = "https://cdn.rawgit.com/martgnz/bcn-geodata/master/barris/barris_geo.json"
+    , what = "sp"
+  )
+
+# check the class of the object
+class( states )
+# [1] "SpatialPolygonsDataFrame"
+# attr(,"package")
+# [1] "sp"
+
+# end of script #
+names(states)
+
+m <- leaflet(states) %>%
+  #setView(-96, 37.8, 4) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
+
+m %>% addPolygons()
+
+
