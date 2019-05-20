6e0d54530eb1f358c5547159ee63feca1f8cdae1
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-20 17:54:50 +0200

    map interactive commented

--- Barcelona_Map.R
+++ Barcelona_Map.R
@@ -18,10 +18,11 @@ class( geojson_bracelona )
 # end of script #
 names(geojson_bracelona)
 
+# Code to call a specific element in the list
 #geojson_bracelona $ N_Barri
 
 
-
+#Create the basic map with districts
 
 m <- leaflet(geojson_bracelona) %>%
   #setView(-96, 37.8, 4) %>%
@@ -29,31 +30,40 @@ m <- leaflet(geojson_bracelona) %>%
     id = "mapbox.light",
     accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
 
+
+#Create variable for the labels, shown when hovering over the different Neighbourhoods
 labels <- sprintf(
   "<strong>Name of Hood: </strong> %s <br/> <strong>Name of District: </strong> %s",
   geojson_bracelona$N_Barri, geojson_bracelona$N_Distri
 ) %>% lapply(htmltools::HTML)
 
 
+#add poligons to the map
+
 m %>% addPolygons(  
+  
+  #creates the dashed line in between the districts
   weight = 2,
   opacity = 1,
   color = "white",
   dashArray = "3",
   fillOpacity = 0.7,
   
+  #defines the properties of the highlightingline when hovering over the different neighbourhoods
   highlight = highlightOptions(
   weight = 5,
   color = "#666",
   dashArray = "",
   fillOpacity = 0.7,
   bringToFront = TRUE
-),
-label = labels,
-labelOptions = labelOptions(
-  style = list("font-weight" = "normal", padding = "3px 8px"),
-  textsize = "15px",
-  direction = "auto")
+  ),
+
+  #creates the labels
+  label = labels,
+  labelOptions = labelOptions(
+    style = list("font-weight" = "normal", padding = "3px 8px"),
+    textsize = "15px",
+    direction = "auto")
 )
 
 
