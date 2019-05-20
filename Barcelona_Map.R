a2873b180f9ab3cf06d20c3d53c067041c1e9f17
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-20 17:48:08 +0200

    map interactive

--- Barcelona_Map.R
+++ Barcelona_Map.R
@@ -5,27 +5,55 @@ library( geojsonio )
 library(leaflet)
 
 # transfrom .json file into a spatial polygons data frame
-states <- 
+geojson_bracelona <- 
   geojson_read( 
     x = "https://cdn.rawgit.com/martgnz/bcn-geodata/master/barris/barris_geo.json"
     , what = "sp"
   )
 
 # check the class of the object
-class( states )
-# [1] "SpatialPolygonsDataFrame"
-# attr(,"package")
-# [1] "sp"
+class( geojson_bracelona )
+
 
 # end of script #
-names(states)
+names(geojson_bracelona)
 
-m <- leaflet(states) %>%
+#geojson_bracelona $ N_Barri
+
+
+
+
+m <- leaflet(geojson_bracelona) %>%
   #setView(-96, 37.8, 4) %>%
   addProviderTiles("MapBox", options = providerTileOptions(
     id = "mapbox.light",
     accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
 
-m %>% addPolygons()
+labels <- sprintf(
+  "<strong>Name of Hood: </strong> %s <br/> <strong>Name of District: </strong> %s",
+  geojson_bracelona$N_Barri, geojson_bracelona$N_Distri
+) %>% lapply(htmltools::HTML)
+
+
+m %>% addPolygons(  
+  weight = 2,
+  opacity = 1,
+  color = "white",
+  dashArray = "3",
+  fillOpacity = 0.7,
+  
+  highlight = highlightOptions(
+  weight = 5,
+  color = "#666",
+  dashArray = "",
+  fillOpacity = 0.7,
+  bringToFront = TRUE
+),
+label = labels,
+labelOptions = labelOptions(
+  style = list("font-weight" = "normal", padding = "3px 8px"),
+  textsize = "15px",
+  direction = "auto")
+)
 
 
