ddd6598f4c292376698b8cff56ea13076aa68cbe
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-21 16:02:44 +0200

    map inside shiney app

--- Barcelona_Map.R
+++ Barcelona_Map.R
@@ -30,56 +30,58 @@ names(geojson_bracelona)
 bins <- c(0, 5, 10, 15, 20, 25, 30, 35, 40, Inf)
 pal <- colorBin("YlOrRd", domain = unemployment$Gener, bins = bins)
 
-
-#Create the basic map with districts
-
-m <- leaflet(geojson_bracelona) %>%
-  #setView(-96, 37.8, 4) %>%
-  addProviderTiles("MapBox", options = providerTileOptions(
-    id = "mapbox.light",
-    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
+###################################################Shiney
 
 
-#Create variable for the labels, shown when hovering over the different Neighbourhoods
-labels <- sprintf(
-  "<strong>Name of Hood: </strong> %s <br/> <strong>Name of District: </strong> %s <br/> <strong>Name of District: </strong> %g",
-  geojson_bracelona$N_Barri, geojson_bracelona$N_Distri, unemployment$Gener
-) %>% lapply(htmltools::HTML)
 
+ui <- fluidPage(
+  leafletOutput("m"),
+  actionButton("recalc", "New points")
+)
 
-#add poligons to the map
-
-m %>% addPolygons(  
+server <- function(input, output, session) {
   
-  #fill of tiles depending on bins and population density
-  fillColor = ~pal(as.numeric(unemployment$Gener)),
   
-  #creates the dashed line in between the districts
-  
-  weight = 2,
-  opacity = 1,
-  color = "white",
-  dashArray = "3",
-  fillOpacity = 0.7,
-  
-  #defines the properties of the highlightingline when hovering over the different neighbourhoods
-  highlight = highlightOptions(
-  weight = 5,
-  color = "#666",
-  dashArray = "",
-  fillOpacity = 0.7,
-  bringToFront = TRUE
-  ),
+  output$m <- renderLeaflet({
+    leaflet(geojson_bracelona) %>%
+      addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))
+      ) 
+    m %>% addPolygons(  
+      
+      #fill of tiles depending on bins and population density
+      fillColor = ~pal(as.numeric(unemployment$Gener)),
+      
+      #creates the dashed line in between the districts
+      
+      weight = 2,
+      opacity = 1,
+      color = "white",
+      dashArray = "3",
+      fillOpacity = 0.7,
+      
+      #defines the properties of the highlightingline when hovering over the different neighbourhoods
+      highlight = highlightOptions(
+        weight = 5,
+        color = "#666",
+        dashArray = "",
+        fillOpacity = 0.7,
+        bringToFront = TRUE
+      ),
+      #creates the labels
+      label = labels,
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", padding = "3px 8px"),
+        textsize = "15px",
+        direction = "auto")) %>%
+      
+      
+      addLegend(pal = pal, values = unemployment$Població.16.64.anys, opacity = 0.7, title = NULL,
+                position = "bottomright")
+      
+    
+  })
+}
 
-  #creates the labels
-  label = labels,
-  labelOptions = labelOptions(
-    style = list("font-weight" = "normal", padding = "3px 8px"),
-    textsize = "15px",
-    direction = "auto")) %>%
-
-
-  addLegend(pal = pal, values = unemployment$Població.16.64.anys, opacity = 0.7, title = NULL,
-            position = "bottomright")
-
-
+shinyApp(ui, server)
