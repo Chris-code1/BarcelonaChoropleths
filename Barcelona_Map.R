9c9c2d311b311a0f42281c66368f7671301d4452
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-29 11:42:44 +0200

    initial map

--- Barcelona_Map.R
+++ Barcelona_Map.R
@@ -1,16 +1,11 @@
-
-
 # load necessary packages
 library( geojsonio )
 library(leaflet)
-library(shiny)
 
 #load in Data
 unemployment <- read.csv("Data/Barcelona_unemployment/2016_unemployment.csv", sep=",")
 
 str(unemployment)
-
-
 
 
 # transfrom .json file into a spatial polygons data frame
@@ -25,63 +20,61 @@ geojson_bracelona <-
 class( geojson_bracelona )
 names(geojson_bracelona)
 
+# Code to call a specific element in the list
+#geojson_bracelona $ N_Barri
 
 
-bins <- c(0, 5, 10, 15, 20, 25, 30, 35, 40, Inf)
-pal <- colorBin("YlOrRd", domain = unemployment$Gener, bins = bins)
-
-###################################################Shiney
+bins <- c(0, 5.000, 10.000, 15.000, 20.000, 25.000, 30.000, 35.000, Inf)
+pal <- colorBin("YlOrRd", domain = unemployment$Població.16.64.anys, bins = bins)
 
 
+#Create the basic map with districts
 
-ui <- fluidPage(
-  leafletOutput("m"),
-  actionButton("recalc", "New points")
-)
+m <- leaflet(geojson_bracelona) %>%
+  #setView(-96, 37.8, 4) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
 
-server <- function(input, output, session) {
+
+#Create variable for the labels, shown when hovering over the different Neighbourhoods
+labels <- sprintf(
+  "<strong>Name of Hood: </strong> %s <br/> <strong>Name of District: </strong> %s <br/> <strong>Name of District: </strong> %g",
+  geojson_bracelona$N_Barri, geojson_bracelona$N_Distri, unemployment$Població.16.64.anys
+) %>% lapply(htmltools::HTML)
+
+
+#add poligons to the map
+
+m %>% addPolygons(  
+  
+  #fill of tiles depending on bins and population density
+  fillColor = ~pal(unemployment$Població.16.64.anys),
+  
+  #creates the dashed line in between the districts
+  
+  weight = 2,
+  opacity = 1,
+  color = "white",
+  dashArray = "3",
+  fillOpacity = 0.7,
+  
+  #defines the properties of the highlightingline when hovering over the different neighbourhoods
+  highlight = highlightOptions(
+    weight = 5,
+    color = "#666",
+    dashArray = "",
+    fillOpacity = 0.7,
+    bringToFront = TRUE
+  ),
+  
+  #creates the labels
+  label = labels,
+  labelOptions = labelOptions(
+    style = list("font-weight" = "normal", padding = "3px 8px"),
+    textsize = "15px",
+    direction = "auto")) %>%
   
   
-  output$m <- renderLeaflet({
-    leaflet(geojson_bracelona) %>%
-      addProviderTiles("MapBox", options = providerTileOptions(
-        id = "mapbox.light",
-        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))
-      ) 
-    m %>% addPolygons(  
-      
-      #fill of tiles depending on bins and population density
-      fillColor = ~pal(as.numeric(unemployment$Gener)),
-      
-      #creates the dashed line in between the districts
-      
-      weight = 2,
-      opacity = 1,
-      color = "white",
-      dashArray = "3",
-      fillOpacity = 0.7,
-      
-      #defines the properties of the highlightingline when hovering over the different neighbourhoods
-      highlight = highlightOptions(
-        weight = 5,
-        color = "#666",
-        dashArray = "",
-        fillOpacity = 0.7,
-        bringToFront = TRUE
-      ),
-      #creates the labels
-      label = labels,
-      labelOptions = labelOptions(
-        style = list("font-weight" = "normal", padding = "3px 8px"),
-        textsize = "15px",
-        direction = "auto")) %>%
-      
-      
-      addLegend(pal = pal, values = unemployment$PoblaciÃ³.16.64.anys, opacity = 0.7, title = NULL,
-                position = "bottomright")
-      
-    
-  })
-}
-
-shinyApp(ui, server)
+  addLegend(pal = pal, values = unemployment$Població.16.64.anys, opacity = 0.7, title = NULL,
+            position = "bottomright")
